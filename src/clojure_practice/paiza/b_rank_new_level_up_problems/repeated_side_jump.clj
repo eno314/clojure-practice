26aaf52a620d0e850739421c135c81047e5fb186
Hiroto Kitamura <kirikonknow@gmail.com>
2023-07-06 05:55:09 +0900

    add calc-extra-moved and main method

--- src/clojure_practice/paiza/b_rank_new_level_up_problems/repeated_side_jump.clj
+++ src/clojure_practice/paiza/b_rank_new_level_up_problems/repeated_side_jump.clj
@@ -1,4 +1,5 @@
-(ns clojure-practice.paiza.b-rank-new-level-up-problems.repeated-side-jump)
+(ns clojure-practice.paiza.b-rank-new-level-up-problems.repeated-side-jump
+  (:require [clojure-practice.paiza.libs :refer [read-int-values-line]]))
 
 (defn count-left-jump [k]
   (int (/ (+ k 1) 4)))
@@ -6,8 +7,17 @@
 (defn is-just-left-point? [k]
   (zero? (mod (+ k 1) 4)))
 
-(defn count-pass-count-between-left-and-center [k]
+(defn count-pass-between-left-and-center [k]
   (let [count (* (count-left-jump k) 2)]
     (if (is-just-left-point? k) (- count 1) count)))
+
+(defn calc-extra-moved [n x k]
+  (let [pass-count (count-pass-between-left-and-center k)
+        pass-count-before-extend (count-pass-between-left-and-center (* n 4))]
+    (* x (- pass-count pass-count-before-extend))))
+
+(defn main []
+  (let [[n x k] (read-int-values-line)]
+    (println (calc-extra-moved n x k))))
 
 ; (main)
--- test/clojure_practice/paiza/b_rank_new_level_up_problems/repeated_side_jump_test.clj
+++ test/clojure_practice/paiza/b_rank_new_level_up_problems/repeated_side_jump_test.clj
@@ -1,6 +1,6 @@
 (ns clojure-practice.paiza.b-rank-new-level-up-problems.repeated-side-jump-test
-  (:require [clojure-practice.paiza.b-rank-new-level-up-problems.repeated-side-jump :refer [count-left-jump
-                                                                                            count-pass-count-between-left-and-center]]
+  (:require [clojure-practice.paiza.b-rank-new-level-up-problems.repeated-side-jump :refer [calc-extra-moved count-left-jump
+                                                                                            count-pass-between-left-and-center]]
             [clojure.test :refer [deftest is]]))
 
 (deftest count-left-jump-test
@@ -22,12 +22,17 @@
   (is (= (count-left-jump 15) 4))
   (is (= (count-left-jump 16) 4)))
 
-(deftest count-pass-count-between-left-and-center-test
-  (is (= (count-pass-count-between-left-and-center 3) 1))
-  (is (= (count-pass-count-between-left-and-center 4) 2))
-  (is (= (count-pass-count-between-left-and-center 6) 2))
-  (is (= (count-pass-count-between-left-and-center 7) 3))
-  (is (= (count-pass-count-between-left-and-center 8) 4))
-  (is (= (count-pass-count-between-left-and-center 10) 4))
-  (is (= (count-pass-count-between-left-and-center 11) 5))
-  (is (= (count-pass-count-between-left-and-center 12) 6)))
+(deftest count-pass-between-left-and-center-test
+  (is (= (count-pass-between-left-and-center 3) 1))
+  (is (= (count-pass-between-left-and-center 4) 2))
+  (is (= (count-pass-between-left-and-center 6) 2))
+  (is (= (count-pass-between-left-and-center 7) 3))
+  (is (= (count-pass-between-left-and-center 8) 4))
+  (is (= (count-pass-between-left-and-center 10) 4))
+  (is (= (count-pass-between-left-and-center 11) 5))
+  (is (= (count-pass-between-left-and-center 12) 6)))
+
+
+(deftest calc-extra-moved-test
+  (is (= (calc-extra-moved 1 3 10) 6))
+  (is (= (calc-extra-moved 2 5 20) 30)))
