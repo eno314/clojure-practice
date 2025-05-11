f7428dd0cde28c35b63a575a84fdd2830be97554
Hiroto Kitamura <kirikonknow@gmail.com>
2025-05-11 15:53:55 +0900

    fix

--- src/clojure_practice/paiza/forest_contest_019/prime_number.clj
+++ src/clojure_practice/paiza/forest_contest_019/prime_number.clj
@@ -1,24 +1,24 @@
 (ns clojure-practice.paiza.forest-contest-019.prime-number)
 
 (defn- divisible? [target divider]
-  (= 0 (mod target divider)))
+  (zero? (mod target divider)))
 
 (defn- prime-number? [target-num]
-  ;(cond
-  ;  (< num 2) false
-  ;  (= num 2) true
-  ;  (even? num) false
-  ;  :else (let [range-limit (inc (long (Math/sqrt num)))
-  ;              target-range (range 3 range-limit 2)]
-  ;          (not-any? #(divisible? num %) target-range))))
-  (loop [numbers (range 2 (inc target-num))]
-    (let [prime-num (first numbers)
-          rest-numbers (rest numbers)]
-      (if (empty? rest-numbers)
-        (= prime-num target-num)
-        (recur (filterv
-                #(not (divisible? % prime-num))
-                rest-numbers))))))
+  (cond
+    (< target-num 2) false
+    (= target-num 2) true
+    (even? target-num) false
+    :else (let [range-limit (inc (long (Math/sqrt target-num)))
+                target-range (range 3 range-limit 2)]
+            (not-any? #(divisible? target-num %) target-range))))
+  ;(loop [numbers (range 2 (inc target-num))]
+  ;  (let [prime-num (first numbers)
+  ;        rest-numbers (rest numbers)]
+  ;    (if (empty? rest-numbers)
+  ;      (= prime-num target-num)
+  ;      (recur (filterv
+  ;              #(not (divisible? % prime-num))
+  ;              rest-numbers))))))
 
 (defn main []
   (as-> (Long/parseLong (read-line)) n
