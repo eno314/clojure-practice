39448e9a26fae80d35b9c7bce0355b24aeeb07b8
Hiroto Kitamura <kirikonknow@gmail.com>
2025-11-05 06:20:07 +0900

    Refactor eratosthenes-sieve implementation for improved clarity and performance; remove unnecessary update-sieve function.

--- src/clojure_practice/paiza/prime_number_primer/is_prime_multi.clj
+++ src/clojure_practice/paiza/prime_number_primer/is_prime_multi.clj
@@ -10,32 +10,23 @@
   (let [line-length (read-int-value-line)]
     (vec (take line-length (repeatedly read-int-value-line)))))
 
-(defn- update-sieve
-  [sieve prime]
-  (loop [multiple (* 2 prime)
-         updated-sieve sieve]
-    (if (> multiple (dec (count sieve)))
-      updated-sieve
-      (recur (+ multiple prime)
-             (assoc updated-sieve multiple false)))))
-
 (defn- eratosthenes-sieve
   [n]
-  (let [init-sieve (vec (concat [false, false]
-                                (vec (repeat (dec n) true))))]
-    (loop [i 2
-           sieve init-sieve]
-      (if (> i n)
-        sieve
-        (if (false? (get sieve i))
-          (recur (inc i) sieve)
-          (recur (inc i) (update-sieve sieve i)))))))
+  (let [sieve (boolean-array (inc n) true)
+        limit (long (Math/sqrt n))]
+    (when (>= n 0) (aset sieve 0 false))
+    (when (>= n 1) (aset sieve 1 false))
+    (doseq [i (range 2 (inc limit))]
+      (when (aget sieve i)
+        (doseq [multiple (range (* i 2) (inc n) i)]
+          (aset sieve multiple false))))
+    sieve))
 
 (defn- exam-numbers-to-results
   [exam-numbers]
   (let [max-number (apply max exam-numbers)
         sieve (eratosthenes-sieve max-number)]
-    (str/join "\n" (map #(if (get sieve %) "pass" "failure") exam-numbers))))
+    (str/join "\n" (map #(if (aget sieve %) "pass" "failure") exam-numbers))))
 
 (defn main
   []
