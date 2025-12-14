fda359981b12c6dc61f9949ec7a463285022d140
Hiroto Kitamura <kirikonknow@gmail.com>
2025-12-15 06:25:22 +0900

    Add nasty-sugoroku implementation and corresponding tests

---
+++ src/clojure_practice/paiza/euclidean_primer/nasty_sugoroku.clj
@@ -0,0 +1,35 @@
+(ns clojure-practice.paiza.euclidean-primer.nasty-sugoroku
+  (:require
+   [clojure.string :as string]))
+
+(defn gcd
+  [[a b]]
+  (loop [x (Math/abs a)
+         y (Math/abs b)]
+    (cond
+      (zero? x) y
+      (zero? y) x
+      (< x y) (recur x (mod y x))
+      :else (recur y (mod x y)))))
+
+(defn- nasty-sugoroku
+  [[n a]]
+  (if (zero? (mod n a))
+    []
+    (filter #(not (or (= a %)
+                      (zero? (mod n %))
+                      (zero? (mod n (gcd [a %])))))
+            (range 1 1001))))
+
+(defn- create-output-str
+  [results]
+  (if (empty? results)
+    "-1"
+    (string/join "\n" results)))
+
+(defn main
+  []
+  (->> (map #(Long/parseLong %) (string/split (read-line) #" "))
+       (nasty-sugoroku)
+       (create-output-str)
+       (println)))
