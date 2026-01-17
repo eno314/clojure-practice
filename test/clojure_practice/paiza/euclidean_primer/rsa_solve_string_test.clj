1ef5c0f0bd0dc128c0efd63145a2bfb706055f4d
Hiroto Kitamura <kirikonknow@gmail.com>
2026-01-17 17:33:19 +0900

    Add RSA string solving implementation and corresponding tests

---
+++ test/clojure_practice/paiza/euclidean_primer/rsa_solve_string_test.clj
@@ -0,0 +1,16 @@
+(ns clojure-practice.paiza.euclidean-primer.rsa-solve-string-test
+  (:require
+   [clojure-practice.paiza.euclidean-primer.rsa-solve-string :refer [main]]
+   [clojure.test :refer [deftest is testing]]))
+
+(deftest rsa-basic-solve-string
+  (testing "sample1"
+    (with-in-str "3995747143 3007 602607029\n"
+      (let [actual (with-out-str (main))
+            expected "PAIZ\n"]
+        (is (= expected actual)))))
+  (testing "sample2"
+    (with-in-str "378925147 2633 253439504\n"
+      (let [actual (with-out-str (main))
+            expected "Nice\n"]
+        (is (= expected actual))))))
